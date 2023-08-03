076f74beac3a19324f034a567a912b9dea9ab2a6
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-08-03 17:48:26 +0100

    Set up files

---
+++ src/components/imgData.js
@@ -0,0 +1,22 @@
+import uuidv4 from 'uuid';
+
+const imgData = [
+  { id: uuidv4(), src: '../assets/img/food1', name: 'food1' },
+  { id: uuidv4(), src: '../assets/img/food2', name: 'food2' },
+  { id: uuidv4(), src: '../assets/img/food3', name: 'food3' },
+  { id: uuidv4(), src: '../assets/img/food4', name: 'food4' },
+  { id: uuidv4(), src: '../assets/img/food5', name: 'food5' },
+  { id: uuidv4(), src: '../assets/img/food6', name: 'food6' },
+  { id: uuidv4(), src: '../assets/img/food7', name: 'food7' },
+  { id: uuidv4(), src: '../assets/img/food8', name: 'food8' },
+  { id: uuidv4(), src: '../assets/img/food9', name: 'food9' },
+  { id: uuidv4(), src: '../assets/img/food10', name: 'food10' },
+  { id: uuidv4(), src: '../assets/img/food11', name: 'food11' },
+  { id: uuidv4(), src: '../assets/img/food12', name: 'food12' },
+  { id: uuidv4(), src: '../assets/img/food13', name: 'food13' },
+  { id: uuidv4(), src: '../assets/img/food14', name: 'food14' },
+  { id: uuidv4(), src: '../assets/img/food15', name: 'food15' },
+  { id: uuidv4(), src: '../assets/img/food16', name: 'food16' },
+];
+
+export default imgData;
